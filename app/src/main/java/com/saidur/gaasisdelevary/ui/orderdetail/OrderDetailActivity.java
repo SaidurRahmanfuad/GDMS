b0617346ace858100eb51a0e209207d0ab04dd51
Saidur Rahman <devsaidur@gmail.com>
2021-03-01 10:00:35 +0600

    Add Order details UI

---
+++ app/src/main/java/com/saidur/gaasisdelevary/ui/orderdetail/OrderDetailActivity.java
@@ -0,0 +1,16 @@
+package com.saidur.gaasisdelevary.ui.orderdetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saidur.gaasisdelevary.R;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+    }
+}
