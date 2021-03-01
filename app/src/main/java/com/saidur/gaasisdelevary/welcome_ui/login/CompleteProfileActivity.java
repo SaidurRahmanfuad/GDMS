9e72cd5ce2852f19c98e312764c59a81ce727474
Saidur Rahman <devsaidur@gmail.com>
2021-03-01 09:42:06 +0600

    First commit with UI

---
+++ app/src/main/java/com/saidur/gaasisdelevary/welcome_ui/login/CompleteProfileActivity.java
@@ -0,0 +1,16 @@
+package com.saidur.gaasisdelevary.welcome_ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saidur.gaasisdelevary.R;
+
+public class CompleteProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_profile);
+    }
+}
---
+++ app/src/main/java/com/saidur/gaasisdelevary/welcome_ui/login/FGPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.saidur.gaasisdelevary.welcome_ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saidur.gaasisdelevary.R;
+
+public class FGPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_g_password);
+    }
+}
